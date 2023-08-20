4a1d387311c04d6afebb83fc3a7df6d303fe85c8
Bruno Tenório <tenoriobn@gmail.com>
2023-08-20 12:00:00 -0300

    Lendo um arquivo json

---
+++ aula4/leitura.js
@@ -0,0 +1,7 @@
+const dados = require("./cliente.json");
+
+// Aqui vai mostrar as propriedades do objeto
+console.log(dados);
+
+// Aqui irá mostrar que o arquivo `json` armazenado em dados é um objeto
+console.log(typeof dados)
